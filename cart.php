22ba956ea27d46ba64761f325aacca342d048b66
luda <liudmyla12@yahoo.fr>
2021-03-31 10:35:04 +0200

    init commit

---
+++ cart.php
@@ -0,0 +1,8 @@
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        TODO : Display shopping cart items from $_SESSION here.
+        <?= $_SESSION['name']; ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
